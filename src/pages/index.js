8b6fa616367f9ba44a3067ebe41484c59e25cc2b
Ilona Zancaner <ilonzanc@Ilonas-MBP.iconos.be>
2022-04-21 17:38:50 +0200

    Add redirect for index

---
+++ src/pages/index.js
@@ -0,0 +1,33 @@
+import React from "react"
+import { graphql, navigate, withPrefix } from "gatsby"
+import { getUserLangKey } from "ptz-i18n"
+
+const RedirectIndex = () => {
+  // Skip build, Browsers only
+  if (typeof window !== "undefined") {
+    const { langs, defaultLangKey } = require("../data/languages.js")
+    const langKey = getUserLangKey(langs, defaultLangKey)
+    const homeUrl = withPrefix(`/${langKey}/`)
+
+    navigate(homeUrl)
+  }
+
+  // It's recommended to add your SEO solution in here for bots
+  // eg. https://github.com/ahimsayogajp/ahimsayoga-gatsby/blob/master/src/pages/index.js#L22
+  return <div />
+}
+
+export default RedirectIndex
+
+export const pageQuery = graphql`
+  query IndexQuery {
+    site {
+      siteMetadata {
+        languages {
+          defaultLangKey
+          langs
+        }
+      }
+    }
+  }
+`
