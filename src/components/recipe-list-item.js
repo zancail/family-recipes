b95974e34b5c7c5837689e01f7320a0434bb3566
Ilona Zancaner <ilonzanc@Ilonas-MBP.iconos.be>
2022-04-21 14:17:24 +0200

    Add save button

--- src/components/recipe-list-item.js
+++ src/components/recipe-list-item.js
@@ -2,21 +2,43 @@ import * as React from "react"
 import { Link } from "gatsby"
 import { renderRichText } from "gatsby-source-contentful/rich-text"
 import { GatsbyImage, getImage } from "gatsby-plugin-image"
+import { useState, useEffect } from "react"
 
 const RecipeListItem = (props) => {
   const title = props.recipe.title || props.recipe.slug
   const image = getImage(props.recipe.image)
+
+  const [isSaved, setIsSaved] = useState(false)
+
+  useEffect(() => {
+    console.log("Save button has updated")
+  }, [isSaved])
+
+  const toggleIsSaved = () => {
+    setIsSaved(!isSaved)
+  }
+
   return (
     <li key={props.recipe.slug} className="col-md-6 col-lg-4 col-xl-3 mb-4">
       <div className="card h-100">
         {props.recipe.image && <GatsbyImage image={image} alt={title} />}
-        <div className="card-body">
-          <h2 className="h4">
-            <Link to={"/" + props.recipe.slug}>{title}</Link>
-          </h2>
-          <div>{renderRichText(props.recipe.intro)}</div>
+        <div className="card-body d-flex flex-column justify-content-between">
+          <div>
+            <h2 className="h4">
+              <Link to={"/" + props.recipe.slug}>{title}</Link>
+            </h2>
+            <div>{renderRichText(props.recipe.intro)}</div>
+          </div>
+          <button
+            type="button"
+            onClick={toggleIsSaved}
+            className={`btn border align-self-end ${
+              isSaved ? "btn-danger" : ""
+            }`}
+          >
+            Save
+          </button>
         </div>
-        <button>Save</button>
       </div>
     </li>
   )
