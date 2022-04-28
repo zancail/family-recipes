408a460aaf24e0b9b3ce79a93e01f97304e677d6
Ilona Zancaner <ilonzanc@Ilonas-MacBook-Pro.local>
2022-04-28 17:28:58 +0200

    Change url stuff for internal links in Button component

--- src/components/Button/index.js
+++ src/components/Button/index.js
@@ -2,9 +2,11 @@ import React from 'react'
 import { graphql, Link } from 'gatsby'
 
 const Button = ({ button }) => {
+  const linkedModel = button.link.linkedModel
+  const url = `/${linkedModel.node_locale}/pages/${linkedModel.slug}`
   return (
     <Link
-      to={`${button.internalLink}`}
+      to={`${url}`}
       className={`btn btn-${button.buttonType[0]} btn-lg px-4 gap-3`}
     >
       {button.text}
@@ -17,7 +19,15 @@ export default Button
 export const query = graphql`
   fragment ContentfulButtonFragment on ContentfulButton {
     text
-    internalLink
+    link {
+      linkedModel {
+        contentful_id
+        title
+        slug
+        node_locale
+        __typename
+      }
+    }
     buttonType
   }
 `
